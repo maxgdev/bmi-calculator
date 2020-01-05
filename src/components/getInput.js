860b30b72e307709de535a1fb8983dbb8f218571
maxgdev <max@apphilia.net>
2020-01-05 00:11:44 +0000

    Added state for height, weight and bmi. Also added input html elements

--- src/components/getInput.js
+++ src/components/getInput.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from  "react";
 
 export default function GetInputs() {
+    const [height, setHeight] = useState('');
+    const [weight, setWeight] = useState('');
+    const [bmi, setBMI] = useState('');
+
     return (
         <>
             <p>Enter height</p>
+                <input
+                    type="number"
+                    placeholder="Height"
+                    name="height"
+                    value={height}
+                    onChange={e => setHeight(e.target.value)}
+                />
             <p>Enter weight</p>
+            <input
+                    type="number"
+                    placeholder="Weight"
+                    name="weight"
+                    value={weight}
+                    onChange={e => setWeight(e.target.value)}
+                />
         </>
     )
 }
