2a3bb853c97a591576338964a4c6ac3d2ba487b9
maxgdev <max@apphilia.net>
2020-01-17 11:07:29 +0000

    Added conversion to feet and lbs and made bmiValue h2 element

--- src/components/outputBMI.js
+++ src/components/outputBMI.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function OutputBMI({ height, weight, bmiValue }) {
     return (
         <>
-            <h2>BMI Value</h2>
-            <p className='bmivalue'>{bmiValue}</p>
+            <h2>BMI Value: {bmiValue}</h2>
             <p>Based on height of {height * 100}cm and weight of {weight} kg</p>
+            <p>Based on height of {(height * 100 * 0.0328084).toFixed(2)} feet and weight of  { (weight * 2.204).toFixed(2)} lbs</p>
         </>
     )
 }
