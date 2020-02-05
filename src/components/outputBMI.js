9aad33b7c2152efc004277744aff2b3460c25921
maxgdev <max@apphilia.net>
2020-02-05 12:56:53 +0000

    Added class based on bmiStatus to color code bmiStatus text outpout

--- src/components/outputBMI.js
+++ src/components/outputBMI.js
@@ -10,7 +10,7 @@ export default function OutputBMI({ height, weight, bmiValue }) {
         } else if (bmiValue > 25){
             bmiStatus = 'Overweight';
         } else if(bmiValue > 18.4) {
-            bmiStatus = 'the correct weight';
+            bmiStatus = 'Ideal';
         } else {
             bmiStatus = 'Underweight';
         }
@@ -19,7 +19,7 @@ export default function OutputBMI({ height, weight, bmiValue }) {
     return (
         <>
             <h2>BMI Value: {bmiValue}</h2>
-            <strong>{`Your are ${displayBMIStatus(bmiValue)}`}</strong>
+            <strong className={displayBMIStatus(bmiValue)}>{`Your are ${displayBMIStatus(bmiValue)}`}</strong>
             <p>Based on height of {height * 100}cm and weight of {weight} kg</p>
             <p>Based on height of {(height * 100 * 0.0328084).toFixed(2)} feet and weight of  { (weight * 2.204).toFixed(2)} lbs</p>
         </>
